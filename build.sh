b7fee8df79e3d3ff69c59a8e8311c66ccfb1a770
Mind-Blown012 <mindblown012@hotmail.com>
2019-03-03 11:02:04 -0800

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
         deps/premake/premake5-linux gmake
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-        deps/premake/premake5-linux xcode
+        deps/premake/premake5-macos xcode
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-        deps/premake/premake5-linux vs2017
+        deps/premake/premake5.exe vs2017
 fi
